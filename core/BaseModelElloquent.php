804986d58f15eeafed7a0f58caec24ec3bfe0305
UnixUser <38821945+Unix-User@users.noreply.github.com>
2022-02-19 21:24:55 -0300

    core

--- core/BaseModelElloquent.php
+++ core/BaseModelElloquent.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 /*
@@ -40,46 +39,3 @@ require_once __DIR__ . "/bootstrap_eloquent.php";
 class BaseModelElloquent extends Model {
     //put your code here
 }
-=======
-<?php
-
-/*
- * The MIT License
- *
- * Copyright 2020 Weverton.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-namespace Core;
-
-use Illuminate\Database\Eloquent\Model;
-
-require_once __DIR__ . "/bootstrap_eloquent.php";
-
-
-/**
- * Description of BaseModelElloquent
- *
- * @author Weverton
- */
-class BaseModelElloquent extends Model {
-    //put your code here
-}
->>>>>>> b0472aa6aa2ca3a79035f2b8ecfec1c3f4e267c0
--- core/Session.php
+++ core/Session.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 /*
@@ -58,64 +57,3 @@ class Session
         unset($_SESSION[$keys]);
     }
 }
-=======
-<?php
-
-/*
- * The MIT License
- *
- * Copyright 2020 Weverton.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-namespace Core;
-
-/**
- * Description of Session
- *
- * @author Weverton
- */
-class Session
-{
-
-    public static function set($key, $value)
-    {
-        $_SESSION[$key] = $value;
-    }
-
-    public static function get($key)
-    {
-        if (isset($_SESSION[$key])){
-            return $_SESSION[$key];
-        }
-        return FALSE;
-    }
-
-    public static function destroy($keys)
-    {
-        if (is_array($keys)){
-            foreach ($keys as $key){
-                unset($_SESSION[$key]);
-            }
-        }    
-        unset($_SESSION[$keys]);
-    }
-}
->>>>>>> b0472aa6aa2ca3a79035f2b8ecfec1c3f4e267c0
